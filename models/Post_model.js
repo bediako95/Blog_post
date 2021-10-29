fee1802e5ad7ad637f97655383754e3b4e028a34
Paa Yaw <p.bediako1999@gmail.com>
2021-10-29 12:33:41 +0000

    Modified files

--- models/Post_model.js
+++ models/Post_model.js
@@ -1,54 +1,53 @@
 import mongoose from "mongoose";
-import { commentSchema } from "./comment-model";
+import { commentSchema } from "./Comment_model";
 const { Schema, model } = mongoose;
 
 export const postSchema = new Schema({
+	//title of post
+	title: {
+		type: String,
+		required: true,
+	},
 
-  //title of post
-  title: {
-    type: String,
-    required: true,
-  },
+	//details of blog
+	details: {
+		type: String,
+	},
 
-  //details of blog
-  details: {
-    type: String,
-  },
+	//date
+	date: {
+		type: Date,
+		default: Date.now(),
+	},
 
-  //date
-  date: {
-    type: Date,
-    default: Date.now(),
-  },
+	//author of post
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "author",
+	},
 
-  //author of post
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "author",
-  },
+	//banner for the post
+	banner: {
+		type: String,
+	},
 
-  //banner for the post
-  banner: {
-    type: String,
-  },
+	//displays all comments on post
+	comments: {
+		type: [commentSchema],
+	},
 
-  //displays all comments on post
-  comments: {
-    type: [commentSchema],
-  },
+	//likes for a specific post
+	likes: {
+		type: Number,
+		default: 0,
+	},
 
-  //likes for a specific post
-  likes: {
-    type: Number,
-    default: 0,
-  },
-
-  //unlikes pertaining to post
-  Unlikes: {
-    type: Number,
-    default: 0,
-  },
+	//unlikes pertaining to post
+	Unlikes: {
+		type: Number,
+		default: 0,
+	},
 });
 
 const postModel = model("post", postSchema);
